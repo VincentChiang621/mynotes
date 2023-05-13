e3ae33c1bde47babb029dab2f0f11481847f2533
Vincent Chiang <yunchenc@umich.edu>
2023-05-13 15:41:12 +0800

    second commit

--- lists.py
+++ lists.py
@@ -1,4 +1,4 @@
-#Lists: ordered, mutable, allows duplicate, and different data types
+#Lists: ordered, mutable, allows duplicate, and different data tp
 
 myList = ["banana", "apple", "cherry", "apple", 5, True]
 print(myList)
